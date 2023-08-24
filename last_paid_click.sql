fdcce902f9d44ef22712cc9b438b0c670afe0b67
nastya0901 <133377810+nastya0901@users.noreply.github.com>
2023-08-25 00:11:14 +0300

    Add files via upload

---
+++ last_paid_click.sql
@@ -0,0 +1,36 @@
+with tab as (
+    select
+        s.visitor_id,
+        visit_date,
+        source as utm_source,
+        medium as utm_medium,
+        campaign as utm_campaign,
+        lead_id,
+        created_at,
+        amount,
+        closing_reason,
+        status_id,
+        rank() over (partition by s.visitor_id order by visit_date desc) as rnk
+    from sessions as s
+    left join leads as l
+        on s.visitor_id = l.visitor_id
+    where
+        medium in (
+            'cpc', 'cpm', 'cpa', 'youtube', 'cpp',
+            'tg'
+        )
+)
+select
+    visitor_id,
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
+from tab
+where rnk = 1
+order by visit_date, utm_source, utm_medium, utm_campaign;
